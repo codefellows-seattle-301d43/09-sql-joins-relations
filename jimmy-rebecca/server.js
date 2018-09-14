85bd7ccae12caecb8c894f8f71f47b15500c8685
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-09-14 09:22:16 -0700

    Add express and pg dependencies to package.json and add sql query to get all articles

--- jimmy-rebecca/server.js
+++ jimmy-rebecca/server.js
@@ -19,12 +19,12 @@ app.use(express.static('./public'));
 
 // REVIEW: These are routes for requesting HTML resources.
 app.get('/new-article', (request, response) => {
-  response.sendFile('new.html', {root: './public'});
+  response.sendFile('new.html', { root: './public' });
 });
 
 // REVIEW: These are routes for making API calls to enact CRUD operations on our database.
 app.get('/articles', (request, response) => {
-  client.query(``)
+  client.query(`SELECT * FROM articles`)
     .then(result => {
       response.send(result.rows);
     })
@@ -37,8 +37,8 @@ app.post('/articles', (request, response) => {
   let SQL = '';
   let values = [];
 
-  client.query( SQL, values,
-    function(err) {
+  client.query(SQL, values,
+    function (err) {
       if (err) console.error(err);
       // REVIEW: This is our second query, to be executed when this first query is complete.
       queryTwo();
@@ -49,8 +49,8 @@ app.post('/articles', (request, response) => {
   values = [];
 
   function queryTwo() {
-    client.query( SQL, values,
-      function(err, result) {
+    client.query(SQL, values,
+      function (err, result) {
         if (err) console.error(err);
 
         // REVIEW: This is our third query, to be executed when the second is complete. We are also passing the author_id into our third query.
@@ -63,8 +63,8 @@ app.post('/articles', (request, response) => {
   values = [];
 
   function queryThree(author_id) {
-    client.query( SQL, values,
-      function(err) {
+    client.query(SQL, values,
+      function (err) {
         if (err) console.error(err);
         response.send('insert complete');
       }
@@ -72,14 +72,14 @@ app.post('/articles', (request, response) => {
   }
 });
 
-app.put('/articles/:id', function(request, response) {
+app.put('/articles/:id', function (request, response) {
   let SQL = '';
   let values = [];
-  client.query( SQL, values )
+  client.query(SQL, values)
     .then(() => {
       let SQL = '';
       let values = [];
-      client.query( SQL, values )
+      client.query(SQL, values)
     })
     .then(() => {
       response.send('Update complete');
@@ -92,7 +92,7 @@ app.put('/articles/:id', function(request, response) {
 app.delete('/articles/:id', (request, response) => {
   let SQL = `DELETE FROM articles WHERE article_id=$1;`;
   let values = [request.params.id];
-  client.query( SQL, values )
+  client.query(SQL, values)
     .then(() => {
       response.send('Delete complete');
     })
@@ -103,7 +103,7 @@ app.delete('/articles/:id', (request, response) => {
 
 app.delete('/articles', (request, response) => {
   let SQL = 'DELETE FROM articles';
-  client.query( SQL )
+  client.query(SQL)
     .then(() => {
       response.send('Delete complete');
     })
@@ -129,7 +129,7 @@ function loadAuthors() {
     JSON.parse(fd).forEach(ele => {
       let SQL = 'INSERT INTO authors(author, author_url) VALUES($1, $2) ON CONFLICT DO NOTHING';
       let values = [ele.author, ele.author_url];
-      client.query( SQL, values );
+      client.query(SQL, values);
     })
   })
 }
@@ -137,9 +137,9 @@ function loadAuthors() {
 // REVIEW: This helper function will load articles into the DB if the DB is empty.
 function loadArticles() {
   let SQL = 'SELECT COUNT(*) FROM articles';
-  client.query( SQL )
+  client.query(SQL)
     .then(result => {
-      if(!parseInt(result.rows[0].count)) {
+      if (!parseInt(result.rows[0].count)) {
         fs.readFile('./public/data/hackerIpsum.json', 'utf8', (err, fd) => {
           JSON.parse(fd).forEach(ele => {
             let SQL = `
@@ -149,7 +149,7 @@ function loadArticles() {
               WHERE author=$5;
             `;
             let values = [ele.title, ele.category, ele.published_on, ele.body, ele.author];
-            client.query( SQL, values )
+            client.query(SQL, values)
           })
         })
       }
